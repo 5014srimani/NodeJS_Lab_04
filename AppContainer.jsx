47b55200c1f0816dbb73953d941e676718d3392a
manoj997 <dharmakumar338@gmail.com>
2019-03-02 12:33:14 +0530

    initial codebase react - devdependencies added

---
+++ AppContainer.jsx
@@ -0,0 +1,12 @@
+"use strict";
+import React ,{Component} from 'react';
+export default class AppContainer extends Component{
+    constructor(props){
+        super(props);
+    }
+     render() {
+        return <div>
+            <h2>Hello Reacts </h2>
+        </div>
+     }
+}
